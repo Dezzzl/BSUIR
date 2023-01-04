3fa6b04cce00877693851620d2259094bd89338a
dan <dezzzl2580@gmail.com>
2023-01-05 00:35:27 +0300

    RR + input check

---
+++ sem1/OAiP/numeric_input/check/check.cpp
@@ -0,0 +1,83 @@
+﻿#include<iostream>
+#include<string>
+#include<math.h>
+using namespace std;
+int check_int() {
+	char max[] = "2147483647";
+	int i, count_of_numbers = 0;
+	char temp[100];
+	int x = 0;
+	bool flag = true;
+	bool predel = true;
+	while (flag) {
+		gets_s(temp);
+		int n = strlen(temp);
+		for (i = 0; i < n; i++) {
+			if (temp[i] >= '0' && temp[i] <= '9') {
+				count_of_numbers++;
+			}
+		}
+		if (count_of_numbers == n && n == 10) {
+			for (i = 0; i < 10; i++) {
+				if (int(temp[i]) > int(max[i])) {
+					predel = false;
+					break;
+				}
+			}
+		}
+		if (count_of_numbers == n && n<=10 && predel==true) {
+			if (temp[0] == '0') { count_of_numbers = 0; predel=true, cout << "try again\n"; }
+			else flag = false;
+		}
+		else { count_of_numbers = 0; cout << "try again\n"; predel = true; }
+	
+		}
+	for (i = 0; i < strlen(temp); i++) {
+		x += int((int(temp[i]) - 48) * pow(10, strlen(temp) - (i + 1)));
+	}
+	return x;
+	}
+	
+
+double check_double() {
+	int i, count_of_points=0, count_of_numbers=0, point=0;
+	char temp[100];
+	double x=0;
+	bool flag=true;
+	while (flag) {
+		gets_s(temp);
+		int n = strlen(temp);
+		for (i = 0; i < n; i++) {
+			if (temp[i] >= '0' && temp[i] <= '9') {
+				count_of_numbers++;
+		}
+			if (temp[i] == '.') {
+				count_of_points++;
+				point = i;
+			}
+		}
+		if (((count_of_points + count_of_numbers == n) && count_of_points == 1)) {
+			if ((temp[0] == '0' && temp[1] == '0') || temp[0] == '.' || temp[n - 1] == '.') { count_of_points = 0; count_of_numbers = 0; cout << "try again\n"; }
+			else flag = false;
+		}
+		else { count_of_points = 0; count_of_numbers = 0; cout << "try again\n"; };
+	
+
+	}
+	for (i = 0; i < point; i++) {
+		x += double((int(temp[i]) - 48.0) * pow(10.0, point-(i+1)));	
+}
+	for (i = point + 1; i < strlen(temp); i++) {
+		x+= double((int(temp[i]) - 48.0) / pow(10.0, (i-point)));
+	}
+	return x;
+}
+int main() {
+	int f1;
+	f1 = check_int();
+	cout << f1<<"\n";
+	double f;
+	f = check_double();
+	cout << f;
+	return 0;
+}
--- sem1/PiOIvIS/RR/RR.cpp
+++ sem1/PiOIvIS/RR/RR.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 using namespace std;
 string tests(int numb) {
     string path;
@@ -14,131 +15,107 @@ string tests(int numb) {
           break;
     case 5: { path = "input5.txt"; }
           break;
-    default: cout << "Invalid input. Therefore , the path is first \n"; path = "input1.txt";
+    case 6: { path = "input6.txt"; }
+          break;
     }
     return path;
 }
-int** file_read(int& n, int& m, string& path) {
+void read(ifstream& out) {
+    if (out.is_open()) {
+        char ch;
+        while (out.get(ch))
+        {
+            cout << ch;
+        }
+    }
+}
+int** file_read(int& n, int& m, string& path, ofstream& out, int& number_of_first, int& number_of_second) {
     ifstream in(path);
     if (in.is_open())
     {
-        int count = 0;
-        int temp;
-        while (!in.eof())
-        {
-            in >> temp;
-            count++;
-        }
-        in.seekg(0, ios::beg);
-        in.clear();
-        int count_space = 0;
-        char symbol;
-        while (!in.eof())
-        {
-            in.get(symbol);
-            if (symbol == ' ') count_space++;
-            if (symbol == '\n') break;
-        }
-        in.seekg(0, ios::beg);
-        in.clear();
-        int** x;
-        n = count / (count_space + 1);
-        m = count_space + 1;
-        x = new int* [n];
-        cout << "the incidence matrix of the original graph\n";
-        for (int i = 0; i < n; i++) x[i] = new int[m];
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < m; j++)
-                in >> x[i][j];
+  
+        in >> n;
+        in >> m;
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++)
-            {
-                cout << x[i][j] << " ";
+        int** x = new int* [n];
+        for (int i = 0; i < n; i++) x[i] = new int[m];
+        if (n > 1) {
+            in >> number_of_first;
+            in >> number_of_second;
+            out << "the incidence matrix of the original graph\n";
+
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < m; j++)
+                    in >> x[i][j];
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++)
+                {
+                    out << x[i][j] << " ";
+                }
+                out << "\n";
             }
-            cout << "\n";
+        }
+        else {
+            out << "the incidence matrix of the original graph";
+            char s;
+            if (in.is_open()) {
+                char ch;
+                while (in.get(ch))
+                {
+                    out << ch;
+                }
+            }
+            out << "\nit is impossible to make a closure because there is only one vertex in the graph\n";
         }
         in.close();
         return x;
     }
     else
     {
-        cout << "The file was not found.";
+        cout << "file not found";
     }
 }
-void check(int** x, int& n, int& m) {
-
+void check(int** x, int& n, int& m, ofstream& out, int& number_of_first, int& number_of_second) {
     int i, j;
-    int number_of_first = 0, number_of_second = 0, number_of_ones = 0, stolb = 0;
-    bool flag = true;
-    for (i = 0; i < m; i++) {
-        for (j = 0; j < n; j++) {
-            if (x[j][i] == 1) { number_of_ones++; }
-        }
-        if (number_of_ones == 2)
-        {
-            stolb = i;
-            for (i = 0; i < n; i++) {
-                if (x[i][stolb] == 1)
-                {
-                    number_of_first = i;
-                    break;
-                }
-            }
-            for (i = number_of_first + 1; i < n; i++) {
-                if (x[i][stolb] == 1)
-                {
-                    number_of_second = i;
-                    break;
-                }
-            }
-            break;
-        }
-        else number_of_ones = 0;
-    }
-    if (!(number_of_first == 0 && number_of_second == 0)) {
+    if (n > 1) {
         for (j = 0; j < m; j++) {
-            if (x[number_of_second][j] == 1) {
-                x[number_of_first][j] = 1;
+            if (x[number_of_second - 1][j] == 1) {
+                x[number_of_first - 1][j] = 1;
             }
         }
-        for (i = number_of_second; i < n - 1; i++) {
-
+        for (i = number_of_second - 1; i < n - 1; i++) {
             x[i] = x[i + 1];
-
-        } ofstream out("Output.txt");
-        cout << "the incidence matrix of the closure graph\n";
+        }
         out << "the incidence matrix of the closure graph\n";
         for (i = 0; i < n - 1; i++) {
             for (j = 0; j < m; j++)
             {
                 out << x[i][j] << " ";
-                cout << x[i][j] << " ";
             }
-            cout << "\n";
             out << "\n";
         }
-
-        cout << "closed vertex is " << number_of_first + 1 << "," << number_of_second + 1;
-        out << "closed vertex is " << number_of_first + 1 << "," << number_of_second + 1;
-        out.close();
+        out << "closed vertex is " << number_of_first << "," << number_of_second << "\n\n";
     }
-    else {
-        ofstream out("Output.txt");
-        cout << "there are no incident vertices in this graph";
-        out << "there are no incident vertices in this graph";
-        out.close();
-    }
-    for (i = 0; i < n - 1; i++) { delete[]x[i]; }
+    for (i = 0; i < n; i++) { delete x[i]; }
     delete[]x;
 }
 int main() {
-    cout << "select path(int from 1 to 5)=";
-    int par_path, m = 0, n = 0;
-    int** a;
-    cin >> par_path;
-    string path = tests(par_path);
-    a = file_read(n, m, path);
-    check(a, n, m);
+    int number_of_first = 0;
+    int number_of_second = 0;
+    int par_path = 1, m = 0, n = 0;
+    ofstream out("Output.txt");
+    for (par_path = 1; par_path <= 6; par_path++)
+    {
+        int** a;
+        out << "TEST-" << par_path << "\n";
+        string path = tests(par_path);
+        a = file_read(n, m, path, out, number_of_first, number_of_second);
+        check(a, n, m, out, number_of_first, number_of_second);
+    }
+    out.close();
+    ifstream in("Output.txt");
+    read(in);
+    in.close();
     return 0;
 }
