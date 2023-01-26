da5cb9d53d603d21fc704380d879d6c72015688a
dan <dezzzl2580@gmail.com>
2023-01-26 20:28:34 +0300

    lab1_OAiP

---
+++ sem2/OAiP/LW1/sem2-recursion/sem2-recursion.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <conio.h>
+using namespace std;
+double sqrt_with_recursion(int n, double a) {
+	return n == 0 ? 0.5 * (1 + a) : 0.5 * (sqrt_with_recursion(n - 1, a) + a / sqrt_with_recursion(n - 1, a));
+}
+double sqrt_without_recursion(int n, double a) {
+	double x= 0.5 * (1 + a);;
+		while (n != 0) {
+			x = 0.5 * (x + a / x);
+			n--;
+		}
+	return x;
+}
+int main() {
+	do {
+		double x;
+		int n;
+		cout << "enter the number\n";
+		cin >> x;
+		cout << "enter the accuracy\n";
+		cin >> n;
+		cout << "sqrt(with recursion)=" << sqrt_with_recursion(n, x);
+		cout << "\nsqrt(without recursion)=" << sqrt_without_recursion(n, x);
+		cout << "\nPress n to exit, any other key to repeat.\n";
+	} while (_getch() != 'n');
+	return 0;
+}
